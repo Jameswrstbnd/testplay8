29e6c0f7ad6a32bcc5a7f8456f658e7573666d3a
James Wolz-Romberger <james.wolzromberger@wrstbnd.com>
2022-03-18 14:13:21 -0500

    more linking

--- android/app/src/appstore/java/com/mendix/nativetemplate/MainActivity.java
+++ android/app/src/appstore/java/com/mendix/nativetemplate/MainActivity.java
@@ -9,6 +9,10 @@ import com.mendix.mendixnative.config.AppUrl;
 import com.mendix.mendixnative.react.MendixApp;
 import com.mendix.mendixnative.react.MxConfiguration;
 
+import android.view.KeyEvent;
+import com.github.kevinejohn.keyevent.KeyEventModule;
+
+
 public class MainActivity extends MendixReactActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
